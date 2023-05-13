8d088e490ca456f62b51ee12822f2fb8f38c981d
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2023-05-13 18:00:01 +0700

    🎨 Add common themes

---
+++ lib/common/themes/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors {
+  static const primaryColor = Color(0xFF5c40cc);
+  static const backgroundColor = Color(0xFFfafafa);
+  static const lightGreyColor = Color(0xFF9698A9);
+  static const yellowColor = Color(0xFFFFA235);
+}
---
+++ lib/common/utils/iamges.dart
@@ -0,0 +1,17 @@
+class AppImages {
+  static const String backgroundPlane = "asset/image/background_plane.png";
+  static const String bali = "asset/image/bali.png";
+  static const String ciliwungLakeDetail1 =
+      "asset/image/ciliwung_lake_detail_1.png";
+  static const String ciliwungLake = "asset/image/ciliwung_lake.png";
+  static const String ciliwungLakeDetail2 =
+      "asset/image/ciliwung_lake_detail_2.png";
+  static const String ciliwungLakeDetail3 =
+      "asset/image/ciliwung_lake_detial_3.png";
+  static const String googleLogo = "asset/image/google_logo.png";
+  static const String nature = "asset/image/nature.jpg";
+  static const String planeLogo = "asset/image/plane_logo.png";
+  static const String profilePhoto = "asset/image/profile_photo.png";
+  static const String roma = "asset/image/roma.png";
+  static const String sydney = "asset/image/sydney.png";
+}
